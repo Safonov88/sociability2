aa440b010a988127b3a340d524450b5fe730f09c
Safonov88 <safonovdmitr@gmail.com>
2017-07-17 18:20:31 +0300

    Update methods.rb

--- methods.rb
+++ methods.rb
@@ -1,22 +1,28 @@
-def total(answer, filename)
-  file = File.new(filename, "r:UTF-8")
-  result = file.readlines
-  file.close
+class ResultDecider
+  attr_reader :results
+  
+  def read_from_file(file_name)
+    file = File.new(filename, "r:UTF-8")
+    results = file.readlines
+    file.close
+  end
 
-  case answer
-  when 0..3
-    result[6].to_s
-  when 4..8
-    result[5].to_s
-  when 9..13
-    result[4].to_s
-  when 14..18
-    result[3].to_s
-  when 19..24
-    result[2].to_s
-  when 25..29
-    result[1].to_s
-  when 30..32
-    result[0].to_s
+  def total(results, answer)
+    case answer
+    when 0..3
+      results[6].to_s
+    when 4..8
+      results[5].to_s
+    when 9..13
+      results[4].to_s
+    when 14..18
+      results[3].to_s
+    when 19..24
+      results[2].to_s
+    when 25..29
+      results[1].to_s
+    when 30..32
+      results[0].to_s
+    end
   end
 end
